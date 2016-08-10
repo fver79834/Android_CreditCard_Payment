74721ac652bede32b541cb92413b2bec952bde9d
Istiak morsalin <istiak.morsalin@sekai-lab-bd.com>
2016-08-10 10:54:31 +0600

    Project Sample

---
+++ android/app/src/main/java/org/commercefactory/braintree/android/SDKActivity.java
@@ -0,0 +1,82 @@
+package org.commercefactory.braintree.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.braintreepayments.api.BraintreePaymentActivity;
+import com.braintreepayments.api.PaymentRequest;
+import com.braintreepayments.api.models.PaymentMethodNonce;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+
+public class SDKActivity extends Activity {
+    private static final String SERVER_BASE = "http://192.168.1.23:3000/"; // Replace with your own server
+    private static final int REQUEST_CODE = Menu.FIRST;
+    private AsyncHttpClient client = new AsyncHttpClient();
+    private String clientToken;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdk);
+
+        getToken();
+    }
+
+    public void onStartClick(View view) {
+        PaymentRequest paymentRequest = new PaymentRequest()
+                .clientToken(clientToken)
+                .amount("$15.00")
+                .primaryDescription("Awesome payment")
+                .secondaryDescription("Using the Client SDK")
+                .submitButtonText("Pay");
+
+        startActivityForResult(paymentRequest.getIntent(this), REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == BraintreePaymentActivity.RESULT_OK) {
+            PaymentMethodNonce paymentMethodNonce = data.getParcelableExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
+
+            RequestParams requestParams = new RequestParams();
+            requestParams.put("payment_method_nonce", paymentMethodNonce.getNonce());
+            requestParams.put("amount", "10.00");
+
+            client.post(SERVER_BASE + "/payment", requestParams, new TextHttpResponseHandler() {
+                @Override
+                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                    Toast.makeText(SDKActivity.this, responseString, Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                    Toast.makeText(SDKActivity.this, responseString, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    private void getToken() {
+        client.get(SERVER_BASE + "/token", new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                findViewById(R.id.btn_start).setEnabled(false);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                clientToken = responseString;
+                findViewById(R.id.btn_start).setEnabled(true);
+            }
+        });
+    }
+}
